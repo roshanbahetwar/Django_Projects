ffcac0744bfca9f569fe6e3681c24d57aae16df5
roshanbahetwar <roshanbahetwar017@.com>
2023-06-06 00:43:51 +0530

    project updated

--- BookMyshow/home/views.py
+++ BookMyshow/home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import HomeDetailsModels
 
 # Create your views here.
+def show(r):
+    prd_list = HomeDetailsModels.objects.all()
+    prd_dict = {'prd_list':prd_list}
+
+    return render(r,'home/show.html',context=prd_dict)
